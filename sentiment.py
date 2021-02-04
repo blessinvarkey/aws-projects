419dc112152fa023b9fed7b5740ffefad229b0e4
Blessin Varkey <blessinvarkey@gmail.com>
2021-02-05 01:19:51 +0530

    Update sentiment.py

--- sentiment.py
+++ sentiment.py
@@ -1,12 +1,15 @@
 #TextBlob
 #Sentiment analysis is the process of detecting positive, negative or neutral sentiment in a text.
 
-from textblob import TextBlob
+from textblob import TextBlob, Word, WordList
 
 #text = input('Write a statement \n')
-text = 'There are many good day'
+text = 'There are many good day.'
+words = TextBlob(text)
+print(words[0:5])
 
-blob = TextBlob(text)
-
-
+#find(sub, start=0, end=9223372036854775807)
 print(blob.replace(blob.words[4], blob.words[4].pluralize()))
+word = Word("octopi")
+lem = word.lemmatize()
+print(lem)
